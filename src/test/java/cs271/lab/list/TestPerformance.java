ec04a4fa8e115416d54e575a7281fa9dc67062de
Konstantin Läufer <laufer@cs.luc.edu>
2017-09-28 10:15:11 -0500

    Update TestPerformance.java

--- src/test/java/cs271/lab/list/TestPerformance.java
+++ src/test/java/cs271/lab/list/TestPerformance.java
@@ -10,10 +10,15 @@ import org.junit.Test;
 
 public class TestPerformance {
 
-	// TODO run test and record running times for SIZE = 10, 100, 1000, 10000
+	// TODO run test and record running times for SIZE = 10, 100, 1000, 10000, ...
+	// (choose in conjunction with REPS below up to an upper limit where the clock 
+	// running time is in the tens of seconds)
+	// TODO refactor to DRY
 	// which of the two lists performs better as the size increases?
 	private final int SIZE = 10;
 
+	// TODO choose this value in such a way that you can observe an actual effect 
+	// for increasing problem sizes
 	private final int REPS = 1000000;
 
 	private List<Integer> arrayList;
