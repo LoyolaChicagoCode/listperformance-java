10b0f47d9478bbf41279715c9a4776130c9f7390
John O'Sullivan <josullivan1@luc.edu>
2017-09-28 15:27:46 +0000

    fixed source formatting
    Signed-off-by: John O'Sullivan <josullivan1@luc.edu>

--- src/test/java/cs271/lab/list/TestIterator.java
+++ src/test/java/cs271/lab/list/TestIterator.java
@@ -8,97 +8,96 @@ import static org.junit.Assert.fail;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestIterator {
 
-	private List<Integer> list;
-	// See the Java List Interface documentation to understand what all the List methods do ...
+  private List<Integer> list;
+  // See the Java List Interface documentation to understand what all the List methods do ...
 
-	@Before
-	public void setUp() throws Exception {
-		list = new ArrayList<Integer>();
-		// TODO also try with a LinkedList - does it make any difference?
-	}
+  @Before
+  public void setUp() throws Exception {
+    list = new ArrayList<Integer>();
+    // TODO also try with a LinkedList - does it make any difference?
+  }
 
-	@After
-	public void tearDown() throws Exception {
-		list = null;
-	}
+  @After
+  public void tearDown() throws Exception {
+    list = null;
+  }
 
-	@Test
-	public void testEmpty() {
-		final Iterator<Integer> i = list.iterator();
-		assertFalse(i.hasNext());
-	}
+  @Test
+  public void testEmpty() {
+    final Iterator<Integer> i = list.iterator();
+    assertFalse(i.hasNext());
+  }
 
-	@Test
-	public void testNonempty() {
-		list.add(33);
-		list.add(77);
-		list.add(44);
-		list.add(77);
-		list.add(55);
-		list.add(77);
-		list.add(66);
-		final Iterator<Integer> i = list.iterator();
-		assertTrue(i.hasNext());
-		assertEquals(33, i.next().intValue());
-		// TODO fix the expected values in the assertions below
-		assertTrue(i.hasNext());
-		assertEquals(0, i.next().intValue());
-		assertTrue(i.hasNext());
-		assertEquals(0, i.next().intValue());
-		assertTrue(i.hasNext());
-		assertEquals(0, i.next().intValue());
-		assertTrue(i.hasNext());
-		assertEquals(0, i.next().intValue());
-		assertTrue(i.hasNext());
-		assertEquals(0, i.next().intValue());
-		assertTrue(i.hasNext());
-		assertEquals(0, i.next().intValue());
-		assertFalse(i.hasNext());
-	}
+  @Test
+  public void testNonempty() {
+    list.add(33);
+    list.add(77);
+    list.add(44);
+    list.add(77);
+    list.add(55);
+    list.add(77);
+    list.add(66);
+    final Iterator<Integer> i = list.iterator();
+    assertTrue(i.hasNext());
+    assertEquals(33, i.next().intValue());
+    // TODO fix the expected values in the assertions below
+    assertTrue(i.hasNext());
+    assertEquals(0, i.next().intValue());
+    assertTrue(i.hasNext());
+    assertEquals(0, i.next().intValue());
+    assertTrue(i.hasNext());
+    assertEquals(0, i.next().intValue());
+    assertTrue(i.hasNext());
+    assertEquals(0, i.next().intValue());
+    assertTrue(i.hasNext());
+    assertEquals(0, i.next().intValue());
+    assertTrue(i.hasNext());
+    assertEquals(0, i.next().intValue());
+    assertFalse(i.hasNext());
+  }
 
-	@Test
-	public void testRemove() {
-		list.add(33);
-		list.add(77);
-		list.add(44);
-		list.add(77);
-		list.add(55);
-		list.add(77);
-		list.add(66);
-		final Iterator<Integer> i = list.iterator();
-		while (i.hasNext()) {
-			if (i.next() == 77) {
-				i.remove(); // TODO what happens if you use list.remove(77)?
-			}
-		}
-		// TODO using assertEquals and Arrays.asList, express which values are left in the list
-		// See TestList.java for examples of how to use Arrays.asList; also see the Java Arrays
-		// class for more information
-		fail("Not yet implemented"); // remove this line when done
-	}
+  @Test
+  public void testRemove() {
+    list.add(33);
+    list.add(77);
+    list.add(44);
+    list.add(77);
+    list.add(55);
+    list.add(77);
+    list.add(66);
+    final Iterator<Integer> i = list.iterator();
+    while (i.hasNext()) {
+      if (i.next() == 77) {
+        i.remove(); // TODO what happens if you use list.remove(77)?
+      }
+    }
+    // TODO using assertEquals and Arrays.asList, express which values are left in the list
+    // See TestList.java for examples of how to use Arrays.asList; also see the Java Arrays
+    // class for more information
+    fail("Not yet implemented"); // remove this line when done
+  }
 
-	@Test
-	public void testAverageValues() {
-		list.add(33);
-		list.add(77);
-		list.add(44);
-		list.add(77);
-		list.add(55);
-		list.add(77);
-		list.add(66);
-		double sum = 0;
-		int n = 0;
-		// TODO use an iterator and a while loop to compute the average (mean) of the values
-		// (defined as the sum of the items divided by the number of items)
-		// testNonempty shows how to use an iterator; use i.hasNext() in the while loop condition
-		assertEquals(61.3, sum / n, 0.1);
-		assertEquals(7, n);
-	}
+  @Test
+  public void testAverageValues() {
+    list.add(33);
+    list.add(77);
+    list.add(44);
+    list.add(77);
+    list.add(55);
+    list.add(77);
+    list.add(66);
+    double sum = 0;
+    int n = 0;
+    // TODO use an iterator and a while loop to compute the average (mean) of the values
+    // (defined as the sum of the items divided by the number of items)
+    // testNonempty shows how to use an iterator; use i.hasNext() in the while loop condition
+    assertEquals(61.3, sum / n, 0.1);
+    assertEquals(7, n);
+  }
 }
